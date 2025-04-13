e8173b9a460ddf77e34a8a277c5885d9d92ae930
David Terra Alves <david@davidterra.com>
2025-04-13 12:28:55 -0300

    feat(user-integration-test): add integration tests for User repository
    - Added `UserRepositoryIntegrationTests` to validate CRUD operations for the `User` entity.
    - Configured `TestDbContext` to include `User` entity mappings.
    - Ensured proper setup and teardown for database context in integration tests.

---
+++ tests/Ambev.DeveloperEvaluation.Integration/TestData/UserTestData.cs
@@ -0,0 +1,38 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Enums;
+using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Integration.TestData
+{
+    public static class UserTestData
+    {
+        private static readonly Faker<User> createUserFaker = new Faker<User>()
+            .RuleFor(u => u.Id, f => Guid.NewGuid())
+            .RuleFor(u => u.Username, f => f.Internet.UserName())
+            .RuleFor(u => u.Password, f => $"Test@{f.Random.Number(100, 999)}")
+            .RuleFor(u => u.Email, f => f.Internet.Email())
+            .RuleFor(u => u.Phone, f => $"+55{f.Random.Number(11, 99)}{f.Random.Number(100000000, 999999999)}")
+            .RuleFor(u => u.Status, f => f.PickRandom(UserStatus.Active, UserStatus.Suspended))
+            .RuleFor(u => u.Role, f => f.PickRandom(UserRole.Customer, UserRole.Admin))
+            .RuleFor(u => u.Name, f => new PersonNameValue(f.Name.FirstName(), f.Name.LastName()))
+            .RuleFor(u => u.Address, f => new AddressValue
+            (
+                f.Address.City(),
+                f.Address.StateAbbr(),
+                f.Address.StreetName(),
+                f.Random.Number(1, 9999).ToString(),
+                f.Address.ZipCode("#####-###"),
+                new GeoLocationValue
+                (
+                    f.Address.Latitude().ToString(),
+                    f.Address.Longitude().ToString()
+                )
+            ));
+
+        public static User GenerateValidEntity()
+        {
+            return createUserFaker.Generate();
+        }
+    }
+}
