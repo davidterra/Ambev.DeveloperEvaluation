92446a5e7913b7aae8fb362cc98443ba37cd9d5b
David Terra Alves <david@davidterra.com>
2025-04-13 14:11:24 -0300

    feat(branch): implement branch-related repository and tests

---
+++ tests/Ambev.DeveloperEvaluation.Integration/TestData/BranchesTestData.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Bogus;
+
+namespace Ambev.DeveloperEvaluation.Integration.Repositories.TestData
+{
+    public class BranchesTestData
+    {
+        private static readonly Faker<Branch> createBranchesFaker = new Faker<Branch>()
+            .RuleFor(u => u.Id, f => f.Random.Int(1, 1000))
+            .RuleFor(u => u.Name, f => f.Company.CompanyName()
+       );
+
+        public static Branch GenerateValidEntity()
+        {
+            return createBranchesFaker.Generate();
+        }
+    }
+}
